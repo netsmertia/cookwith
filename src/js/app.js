99dae827a0e4c0658e7d1deba6eb0edbb786f5e7
Natwar <nets.developer@gmail.com>
2020-03-20 09:44:04 +0530

    recipe of the day

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,20 @@
 // require('bootstrap');
+
+$(document).ready(function(){
+    $("#main-slider").owlCarousel({
+        items: 6,
+        startPosition: 0,
+        margin: 20,
+        dots: false,
+        loop: true,
+        autoplay: true,
+        nav: true,
+        pagination :true,
+        autoplayTimeout:6000,
+        responsiveClass:true,
+        navText: ['yes', 'no'],
+    });
+
+    var image = document.getElementsByClassName('parallax');
+    new simpleParallax(image);
+  });
