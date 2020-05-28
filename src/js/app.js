2e723423903ae661b762e59599ed10373a54532d
Natwar <nets.developer@gmail.com>
2020-05-28 20:20:46 +0530

    slick slider added but not working

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,61 @@
 // require('bootstrap');
 
 $(document).ready(function(){
-    $("#main-slider").owlCarousel({
-        items: 6,
-        startPosition: 0,
-        margin: 20,
-        dots: false,
-        loop: true,
-        autoplay: true,
-        nav: true,
-        pagination :true,
-        autoplayTimeout:6000,
-        responsiveClass:true,
-        navText: ['yes', 'no'],
+    var image = document.getElementsByClassName('parallax');
+    // new simpleParallax(image);
+    $('#main-slider').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+  });
+
+
+$(document).ready(function() {
+    $(window).on('scroll', function (e) {
+        $('#main-top-nav').toggleClass('mini-nav', $(window).scrollTop() > 150);
     });
 
-    var image = document.getElementsByClassName('parallax');
-    new simpleParallax(image);
-  });
+    //open full screen search
+    $('#open-full-screen-search').on("click", function(event) {
+        event.preventDefault();
+        $('.full-screen-search').addClass("open");
+    });
+    //close full screen search
+    $('#close-full-screen-search').on("click", function(event) {
+        event.preventDefault();
+        $('.full-screen-search').removeClass("open");
+    });
+
+
+})
